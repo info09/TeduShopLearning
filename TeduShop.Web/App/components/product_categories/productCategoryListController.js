1f03f72b3c92b5518ef7f532266b7ae110a1c49a
Quang Huy Trần <traquahu1997@gmail.com>
2020-04-14 21:57:27 +0700

    delete item

--- TeduShop.Web/App/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/App/components/product_categories/productCategoryListController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService', '$ngBootbox'];
 
-    function productCategoryListController($scope, apiService, notificationService) {
+    function productCategoryListController($scope, apiService, notificationService, $ngBootbox) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -11,9 +11,26 @@
         $scope.getProductCagories = getProductCagories;
 
         $scope.search = search;
+        $scope.deleteProductCategory = deleteProductCategory;
 
         function search() {
             getProductCagories();
+        }
+
+        function deleteProductCategory(id) {
+            $ngBootbox.confirm('Bạn có chắc muốn xóa?').then(function () {
+                var config = {
+                    params: {
+                        id: id
+                    }
+                }
+                apiService.del('api/ProductCategory/Delete', config, function () {
+                    notificationService.displaySuccess('Xóa thành công');
+                    search();
+                }, function () {
+                        notificationService.displayError('Xóa không thành công');
+                });
+            });
         }
 
         function getProductCagories(page) {
